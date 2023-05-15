9d3b79680bb20bff6272a691ef7851f376af7c1c
vnyoon <vnyoon@163.com>
2023-05-15 14:29:32 +0800

    feat: SEO相关的配置

---
+++ A_Nuxt3/04_SEO相关配置/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <NuxtPage />
+  </div>
+</template>
+
+
+<style>
+  a {
+    margin: 0 8px;
+  }
+  a.router-link-active {
+    color: #ff434f;
+  }
+</style>
---
+++ A_Nuxt3/04_SEO相关配置/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script setup>
+  // 设置HTML中Head标签的全部内容
+  useHead({
+    title: "这是网站的标题",
+    viewport: "width=device-width,initial-scale=1,maximum-scale=1 ",
+    meta: [
+      { name: "keywords", content: "这是网站的关键字" },
+      { name: "description", content: "这是网站的一段很长很长很长很长很长的描述" },
+    ]
+  })
+</script>
