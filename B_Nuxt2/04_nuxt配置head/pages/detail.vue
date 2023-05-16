d842979c6814cc1470f5c0ef592fe7323bd5962e
vnyoon <vnyoon@163.com>
2023-05-16 13:54:21 +0800

    feat: nuxt配置全局head和局部head

---
+++ B_Nuxt2/04_nuxt配置head/pages/detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>query - id: {{ $route.query.id }}</h2>
+    <h2>params - id: {{ $route.params.id }}</h2>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'default'
+      }
+    },
+    validate({ params, query }) {
+      console.log(params, query, 'validate');
+
+      // 必须是number类型
+      return /^\d+$/.test(query.id);
+    },
+  }
+</script>
+
---
+++ B_Nuxt2/04_nuxt配置head/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>简介</h2>
+
+    <nuxt-link :to="{ name: 'detail', query: { id: 321312 }, params: { id: 123654 } }" >
+      nuxtLikn - 详情
+    </nuxt-link>
+    <nuxt-link to="/about">关于我们</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IndexPage',
+    data() {
+      return {
+        title: '公司简介'
+      }
+    },
+    head() {
+      return {
+        title: this.title,
+        meta: [
+          {
+            hid: 'keywords',
+            name: 'keywords',
+            content: '公司简介关键字'
+          },
+          {
+            hid: 'description',
+            name: 'description',
+            content: '公司简介很长很长很长的描述'
+          }
+        ]
+      }
+    }
+  }
+</script>
