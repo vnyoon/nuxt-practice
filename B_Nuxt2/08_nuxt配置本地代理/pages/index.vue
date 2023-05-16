60eb62d875f790707f52c2eea00dfc26b4569aaa
vnyoon <vnyoon@163.com>
2023-05-16 16:45:22 +0800

    feat: 使用@nuxtjs/proxy配置本地代理

---
+++ B_Nuxt2/08_nuxt配置本地代理/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>IndexPage</h2>
+
+    <ul>
+      <li v-for="item in list">
+        {{ item.imageName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IndexPage',
+    data() {
+      return {
+        list: [],
+        fetchData: []
+      }
+    },
+    async asyncData({ $axios }) {
+      // http://testapi.xuexiluxian.cn/api/slider/getSliders
+      const res = await $axios.get('/api/slider/getSliders');
+      console.log(res);
+
+      return {
+        list: res.data.data.list || []
+      };
+    }
+  }
+</script>
